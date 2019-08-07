968fef1e4b5ef736d5a047551c32d64aae66d23b
fernando.braz <fbrazcosta@gmail.com>
2019-08-06 22:52:57 -0300

    Pequeno andamento na aula de autenticação...

--- src/main/java/com/fernando/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/fernando/cursomc/security/JWTAuthenticationFilter.java
@@ -7,12 +7,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	
+	
+	private AuthenticationManager authenticationManager;
+	
+	private JWTUtil jwtUtil;
+	
 
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+	
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
